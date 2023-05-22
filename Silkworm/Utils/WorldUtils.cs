a0204c9c93dd409fd41e59ff0e8dbdf1fe91a2d3
iZastic <izastic@gmail.com>
2023-05-22 15:24:44 -0600

    WorldUtils

--- Silkworm/Utils/WorldUtils.cs
+++ Silkworm/Utils/WorldUtils.cs
@@ -1,52 +1,54 @@
 ﻿using Unity.Entities;
+using System.Linq;
+using System.Collections.Generic;
+using System;
 
 namespace Silkworm.Utils;
 
 #nullable enable
 public static class WorldUtils
 {
+    private static readonly string _ClientWorldName = "Client_0";
+    private static readonly string _ServerWorldName = "Server";
     private static World? _ClientWorld;
-    private static bool _ClientWorldExists;
+    private static World? _ServerWorld;
 
-    public static World DefaultWorld
-    {
-        get => World.DefaultGameObjectInjectionWorld;
-    }
+    public static bool ClientWorldExists => WorldExists(_ClientWorldName);
+    public static bool ServerWorldExists => WorldExists(_ServerWorldName);
 
-    public static World? ClientWorld
+    public static World DefaultWorld => World.DefaultGameObjectInjectionWorld;
+    public static World ClientWorld
     {
         get
         {
-            FindClientWorld();
+            if (_ClientWorld == null)
+                _ClientWorld = FindWorld(_ClientWorldName) ?? throw new Exception("Client world does not exist yet");
             return _ClientWorld;
         }
     }
-
-    public static bool ClientWorldExists
+    public static World ServerWorld
     {
         get
         {
-            FindClientWorld();
-            return _ClientWorldExists;
+            if (_ServerWorld == null)
+                _ServerWorld = FindWorld(_ServerWorldName) ?? throw new Exception("Server world does not exist yet");
+            return _ServerWorld;
         }
+    }
+
+    public static bool WorldExists(string name)
+    {
+        foreach (var world in World.All)
+            if (world.Name == name)
+                return true;
+        return false;
     }
 
     public static World? FindWorld(string name)
     {
         foreach (var world in World.All)
-        {
             if (world.Name == name)
                 return world;
-        }
         return null;
-    }
-
-    private static void FindClientWorld()
-    {
-        if (!_ClientWorldExists)
-        {
-            _ClientWorld = FindWorld("Client_0");
-            _ClientWorldExists = _ClientWorld != null;
-        }
     }
 }
